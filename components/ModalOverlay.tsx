181005e0a1084b8ba2d300a1eee6c8367e910be2
pontakornth <pontakorn61@gmail.com>
2021-09-22 19:44:57 +0700

    fix: Make modal overlay work

--- components/ModalOverlay.tsx
+++ components/ModalOverlay.tsx
@@ -1,12 +1,13 @@
 import { tw } from "twind"
 import useEditor from "../stores/useEditor"
 
-const ModalOverlay: React.FC = () => {
+const ModalOverlay: React.FC = ({ children }) => {
 	const isOpen = useEditor(state => state.isOpen)
 	return (
 		<>
 		{isOpen && 
 			<div className={tw`z-40 absolute top-0 bottom-0 left-0 right-0 bg-gray-400 bg-opacity-50`}>
+				{ children }
 			</div>
 		}
 		</>
